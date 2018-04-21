3bc84f9e0ff259eaed497b08111ac9526f04ef1e
Sebastian Artz <sebastian.artz85@gmail.com>
2018-04-21 13:04:41 +0200

    add portrait frame and character texts

--- game.js
+++ game.js
@@ -47,6 +47,7 @@ var imageLineHeight = gameHeight*0.01;
 
 var buttonResourcesWidth = playerCanvasWidth * 0.8;
 var buttonResourcesOffsetY = playerCanvasHeight * 0.1;
+var buttonResourcesOffsetX = buttonResourcesOffsetY;
 var buttonResourcesHeight =  (playerCanvasHeight - 6*buttonResourcesOffsetY) / 5;
 var textButtonWidth = buttonResourcesWidth;
 var textButtonHeight = buttonResourcesHeight;
@@ -54,14 +55,28 @@ var textButtonHeight = buttonResourcesHeight;
 var char0, char1, char2;
 
 class Button {
-    constructor(name, posX, posY, width, height, player) {
+    constructor(name, posX, posY, width, height, player, mode) {
         //this.buttonResourcesWidth = buttonResourcesWidth;
         //this.buttonHeight = buttonResourcesHeight;
-        this.button = game.add.button(posX, posY, 'button', player.increaseXP(), this, 3, 2, 1, 0);
-        this.button.width = width;
-        this.button.height = height;
-        this.text = game.add.text(posX, posY, name, { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
-        this.text.setTextBounds(0, 0, textButtonWidth, textButtonHeight + textShiftY);
+        switch (mode) {
+            case 0:
+                this.button = game.add.button(posX, posY, 'button', player.levelUp, player, 3, 2, 1, 0);
+                break;
+            case 1:
+                this.button = game.add.button(posX, posY, 'button', player.useSkill, player, 3, 2, 1, 0);
+                break;
+            case 2:
+                this.button = game.add.button(posX, posY, 'button', player.increaseXP, player, 3, 2, 1, 0);
+                break;
+            default:
+        }
+        if (this.button != null) {
+            this.button.width = width;
+            this.button.height = height;
+            this.text = game.add.text(posX, posY, name, { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
+            this.text.setTextBounds(0, 0, textButtonWidth, textButtonHeight + textShiftY);
+        }
+
     }
 }
 
@@ -83,22 +98,46 @@ class Character {
         this.imageCanvas.height = this.innerCanvasHeight;
 
         this.buttonX = this.innerCanvasX + this.innerCanvasWidth/2 - buttonResourcesWidth/2;
+        // TODO: fix border
+        this.ButtonSmallX = this.buttonX + 0.5 * this.innerCanvasWidth;
+        this.buttonSmallWidth = 0.5 * textButtonWidth;
 
-        this.buttonLevelUp = new Button("Level Up", this.buttonX, this.getButtonY(2) , buttonResourcesWidth, buttonResourcesHeight, this);
-        this.buttonUseAbility = new Button("Use Ability", this.buttonX, this.getButtonY(3) , buttonResourcesWidth, buttonResourcesHeight, this);
-        this.buttonTRaining = new Button("Trainging", this.buttonX, this.getButtonY(4) , buttonResourcesWidth, buttonResourcesHeight, this);
+        this.textXp = game.add.text(this.ButtonSmallX, this.getButtonY(0), "XP: 0", { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
+        this.textXp.setTextBounds(0, 0, this.buttonSmallWidth, textButtonHeight + textShiftY);
 
+        this.textBonus = game.add.text(this.ButtonSmallX, this.getButtonY(1), "No Bonus", { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
+        this.textBonus.setTextBounds(0, 0, this.buttonSmallWidth, textButtonHeight + textShiftY);
 
-    }
+        this.portraitCanvas = game.add.sprite(this.innerCanvasX + buttonResourcesOffsetX, this.innerCanvasY + buttonResourcesOffsetY, 'canvas');
+        this.portraitCanvas.width = 0.5 * buttonResourcesWidth;
+        this.portraitCanvas.height = 2 * buttonResourcesHeight + buttonResourcesOffsetY;
 
-    increaseXP() {
-        this.cp += this.xpIncrease;
+        this.buttonLevelUp = new Button("Level Up", this.buttonX, this.getButtonY(2) , buttonResourcesWidth, buttonResourcesHeight, this, 0);
+        this.buttonUseAbility = new Button("Use Ability", this.buttonX, this.getButtonY(3) , buttonResourcesWidth, buttonResourcesHeight, this, 1);
+        this.buttonTraining = new Button("Trainging", this.buttonX, this.getButtonY(4) , buttonResourcesWidth, buttonResourcesHeight, this, 2);
+
     }
 
     getButtonY(number) {
         return this.innerCanvasY + buttonResourcesOffsetY + number*(buttonResourcesOffsetY + buttonResourcesHeight);
     }
 }
+
+Character.prototype.increaseXP = function () {
+    this.xp += this.xpIncrease;
+}
+
+Character.prototype.useSkill = function () {
+
+}
+
+Character.prototype.levelUp = function () {
+
+}
+
+Character.prototype.update = function () {
+    this.textXp.text = "XP: " + this.xp;
+};
 
 function preload() {
     console.log("preload");
@@ -128,6 +167,9 @@ function create() {
 
 function update() {
     textGold.text = globalGold;
+    char0.update();
+    char1.update();
+    char2.update();
 }
 
 function actionOnClick() {
