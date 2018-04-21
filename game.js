7127d13ca619ae3fc6936431e084848c41e41d39
Sebastian Artz <sebastian.artz85@gmail.com>
2018-04-21 12:22:08 +0200

    First layout of players canvas

--- game.js
+++ game.js
@@ -1,59 +1,135 @@
-//var game = new Phaser.Game(1600, 900, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 console.log("start");
-var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio,
-    Phaser.CANVAS, 'Horror Clicker', { preload: preload, create: create });
+var targetWidth = 1600;
+var targetHeight = 900;
 
-var image;
-var button;
-var text;
-var counter = 0;
-var buttonWidth = 160;
-var buttonHeight = 100;
+//var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio,
+//    Phaser.CANVAS, 'Horror Clicker', { preload: preload, create: create , update: update});
+var game = new Phaser.Game(targetWidth, targetHeight,
+    Phaser.CANVAS, 'Horror Clicker', { preload: preload, create: create , update: update });
+
+//var button;
+var imageCanvas;
+var imageLine;
+
+var gameWidth = targetWidth; //window.innerWidth * window.devicePixelRatio;
+var gameHeight = targetHeight; //window.innerHeight * window.devicePixelRatio;
+console.log("wh: ", gameWidth, gameHeight)
+
+var verticalPosFactorGold = 0.9;
+
+var textGold;
+var textGoldWidth = 100;
+var textGoldHeight = 100;
+var textGoldPosX = gameWidth / 12;
+var textGoldPosY= gameHeight*verticalPosFactorGold - textGoldHeight;
 var textShiftY = 8;
-var buttonPosX = window.innerWidth / 6;
-var buttonPosY = window.innerHeight*0.95 - buttonWidth;
+var textColor = "#3C1E00";
+var textFont = "32px Arial";
 
-var char1;
+var globalGold = 0;
+var goldIncrease = 1;
+
+//var imageCanvasWidth = gameWidth*0.25;
+//var imageCanvasHeight = gameHeight*0.45;
+
+var statusCanvasWidth = gameWidth*0.1;
+var statusCanvasHeight = gameHeight*0.5;
+console.log("line: ", statusCanvasHeight);
+var playersCanvasPosX = statusCanvasWidth;
+var playersCanvasPosY = statusCanvasHeight;
+var playersCanvasWidth = gameWidth - statusCanvasWidth;
+var playersCanvasHeight = gameHeight - statusCanvasHeight;
+var playerCanvasWidth = playersCanvasWidth / 3;
+var playerCanvasHeight = playersCanvasHeight;
+
+var imageLineWith = gameWidth* 0.98;
+var imageLineHeight = gameHeight*0.01;
+
+var buttonResourcesWidth = playerCanvasWidth * 0.8;
+var buttonResourcesOffsetY = playerCanvasHeight * 0.1;
+var buttonResourcesHeight =  (playerCanvasHeight - 6*buttonResourcesOffsetY) / 5;
+var textButtonWidth = buttonResourcesWidth;
+var textButtonHeight = buttonResourcesHeight;
+
+var char0, char1, char2;
 
 class Button {
-    constructor(name) {
-        this.buttonWidth = buttonWidth;
-        this.buttonHeight = buttonHeight;
+    constructor(name, posX, posY, width, height, player) {
+        //this.buttonResourcesWidth = buttonResourcesWidth;
+        //this.buttonHeight = buttonResourcesHeight;
+        this.button = game.add.button(posX, posY, 'button', player.increaseXP(), this, 3, 2, 1, 0);
+        this.button.width = width;
+        this.button.height = height;
+        this.text = game.add.text(posX, posY, name, { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
+        this.text.setTextBounds(0, 0, textButtonWidth, textButtonHeight + textShiftY);
     }
 }
 
 class Character {
-    constructor(name, index) {
+    constructor(name, playerNum) {
         this.name = name;
-        this.button = new Button();
+        this.playerNum = playerNum;
+        this.xp = 0;
+        this.xpIncrease = 1;
+        this.borderX = 0.1;
+        this.borderY = 0.05;
+        this.innerCanvasX = playersCanvasPosX + playerNum*playerCanvasWidth + playerCanvasWidth*this.borderX/2;
+        this.innerCanvasY = playersCanvasPosY  + playerCanvasHeight*this.borderY/2;
+        this.innerCanvasWidth = playerCanvasWidth * (1-this.borderX);
+        this.innerCanvasHeight = playerCanvasHeight * (1-this.borderY);
+        this.imageCanvas = game.add.sprite(this.innerCanvasX, this.innerCanvasY, 'canvas');
+        //console.log(playersCanvasPosX + playersCanvasWidth, playersCanvasPosY + playersCanvasHeight);
+        this.imageCanvas.width = this.innerCanvasWidth;
+        this.imageCanvas.height = this.innerCanvasHeight;
+
+        this.buttonX = this.innerCanvasX + this.innerCanvasWidth/2 - buttonResourcesWidth/2;
+
+        this.buttonLevelUp = new Button("Level Up", this.buttonX, this.getButtonY(2) , buttonResourcesWidth, buttonResourcesHeight, this);
+        this.buttonUseAbility = new Button("Use Ability", this.buttonX, this.getButtonY(3) , buttonResourcesWidth, buttonResourcesHeight, this);
+        this.buttonTRaining = new Button("Trainging", this.buttonX, this.getButtonY(4) , buttonResourcesWidth, buttonResourcesHeight, this);
+
+
+    }
+
+    increaseXP() {
+        this.cp += this.xpIncrease;
+    }
+
+    getButtonY(number) {
+        return this.innerCanvasY + buttonResourcesOffsetY + number*(buttonResourcesOffsetY + buttonResourcesHeight);
     }
 }
 
 function preload() {
     console.log("preload");
-    console.log(window.outerWidth);
     game.load.image("button", "assets/img/button.png");
+    game.load.image("canvas", "assets/img/canvas.png");
+    game.load.image("line", "assets/img/line.png");
     console.log("preload finished");
+    console.log(game.width, game.height)
 }
 
 function create() {
     game.stage.backgroundColor = '#D4C7BB';
-    //image = game.add.sprite(0, 0, 'button');
-    button = game.add.button(buttonPosX, buttonPosY, 'button', actionOnClick, this, 3, 2, 1, 0);
-    text = game.add.text(button.left, button.top, "0", { font: "32px Arial", fill: "#3C1E00", boundsAlignH: "center", boundsAlignV: "middle" });
-    text.setTextBounds(0, 0, buttonWidth, buttonHeight + textShiftY);
 
-    char1 = new Character("name", 0);
+    imageLine = game.add.sprite((gameWidth - imageLineWith) / 2 , gameHeight*0.5, 'line');
+    imageLine.width = imageLineWith;
+    imageLine.height = imageLineHeight;
+
+    textGold = game.add.text(textGoldPosX, textGoldPosY, "0", { font: textFont, fill: textColor, boundsAlignH: "center", boundsAlignV: "middle" });
+    textGold.setTextBounds(0, 0, textGoldWidth, textGoldHeight + textShiftY);
+
+    char0 = new Character("name1", 0);
+    char1 = new Character("name2", 1);
+    char2 = new Character("name3", 2);
 
     console.log("create");
 }
 
 function update() {
-
+    textGold.text = globalGold;
 }
 
 function actionOnClick() {
-    counter++;
-    text.text = counter;
-    //background.visible =! background.visible;
+    globalGold += goldIncrease;
 }
